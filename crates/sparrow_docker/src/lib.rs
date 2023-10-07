e58fc52b0fb9645a3bcd260f7b6c3ad4aab485b6
Aren Marsden <aren.marsden6@gmail.com>
2023-10-07 13:37:50 +0100

    Add more docker functions
    Signed-off-by: Aren Marsden <aren.marsden6@gmail.com>

--- crates/sparrow_docker/src/lib.rs
+++ crates/sparrow_docker/src/lib.rs
@@ -1,8 +1,19 @@
-use bollard::{Docker, image::ListImagesOptions, service::ImageSummary};
-use lazy_static::lazy_static;
+use bollard::{Docker, image::ListImagesOptions, service::{ImageSummary, ContainerSummary}, container::{ListContainersOptions, StatsOptions, Stats}};
 
+
+use futures::{TryStreamExt, TryFutureExt};
+use lazy_static::lazy_static;
 lazy_static! {
     pub static ref DOCKER: Docker = Docker::connect_with_local_defaults().unwrap();
+}
+
+async fn list_containers() -> Vec<ContainerSummary> {
+    let containers = &DOCKER.list_containers(Some(ListContainersOptions::<String> {
+        all: true,
+        ..Default::default()
+    })).await.unwrap();
+
+    containers.clone()
 }
 
 async fn list_images() -> Vec<ImageSummary> {
@@ -15,6 +26,15 @@ async fn list_images() -> Vec<ImageSummary> {
     images.clone()
 }
 
+async fn get_container_stats(id: String) -> Vec<Stats> {
+    let stats = &DOCKER.stats(&id, Some(StatsOptions {
+        stream: true,
+        ..Default::default()
+     })).try_collect::<Vec<_>>().await.unwrap();
+
+     stats.clone()
+}
+
 #[cfg(test)]
 mod tests {
     use std::collections::HashMap;
@@ -22,10 +42,29 @@ mod tests {
     use super::*;
 
     #[tokio::test]
-    async fn testing() -> Result<(), Box<dyn std::error::Error>> {
+    async fn test_container_stats() -> Result<(), Box<dyn std::error::Error>> {
+        let containers = list_containers();
+        for container in containers.await.into_iter() {
+            let stats = get_container_stats(container.id.unwrap()).await;
+            println!("{:#?}", stats)
+        }
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_image_summary() -> Result<(), Box<dyn std::error::Error>> {
         let images = list_images();
         for image in images.await.into_iter() {
             println!("{:#?}", image)
+        }
+        Ok(())
+    }
+    
+    #[tokio::test]
+    async fn test_container_list() -> Result<(), Box<dyn std::error::Error>> {
+        let containers = list_containers();
+        for container in containers.await.into_iter() {
+            println!("{:#?}", container)
         }
         Ok(())
     }
